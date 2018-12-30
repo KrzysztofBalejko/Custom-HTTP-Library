0443509e55d9096d52efc87d44c94e37bae05bab
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-30 16:57:04 +0000

    HTTP POST Request

--- Fetch/appV2.js
+++ Fetch/appV2.js
@@ -1,6 +1,18 @@
 const http = new EasyHTTP;
 
-//Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
+// Get Users
+/*http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => console.log(data))
+.catch(err => console.log(err)); */
+
+// User Data
+const data = {
+  name: 'John Kowalsky',
+  username: 'JohnKowalsky',
+  email: 'john@gmail.com'
+}
+
+http.post('https://jsonplaceholder.typicode.com/users', data)
 .then(data => console.log(data))
 .catch(err => console.log(err));
+
--- Fetch/easyhttpV2.js
+++ Fetch/easyhttpV2.js
@@ -9,13 +9,30 @@
 
 class EasyHTTP {
 
-// Make an HTTP GET Request
-get(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => resolve(data))
-    .catch(err => reject(err));
-  });
- }
+  // Make an HTTP GET Request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP POST Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+         'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+  }
+  
 }
